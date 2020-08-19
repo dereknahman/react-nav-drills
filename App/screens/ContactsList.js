d0ef0e179e0df7ad4d229d2dc69f55e753b6050d
dereknahman <44810431+dereknahman@users.noreply.github.com>
2020-08-19 22:03:50 +0100

    first commit

---
+++ App/screens/ContactsList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {FlatList} from 'react-native';
+
+import {Row, Separator} from '../components/Row';
+import users from '../data/users';
+import ContactDetails from './ContactDetails';
+
+export default ({navigation}) => (
+  <FlatList
+    data={users}
+    keyExtractor={(item) => {
+      return `${item.id.value}-${item.phone}`;
+    }}
+    renderItem={({item}) => {
+      const name = `${item.name.first} ${item.name.last}`;
+
+      return (
+        <Row
+          image={{uri: item.picture.thumbnail}}
+          title={name}
+          subtitle={item.email}
+          onPress={() => navigation.push('ContactDetails', {contact: item})}
+        />
+      );
+    }}
+    ItemSeparatorComponent={Separator}
+    ListHeaderComponent={() => <Separator />}
+    ListFooterComponent={() => <Separator />}
+    contentContainerStyle={{paddingVertical: 20}}
+  />
+);
